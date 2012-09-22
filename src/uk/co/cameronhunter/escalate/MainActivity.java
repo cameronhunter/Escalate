c2ad6fe8d61c1e59128580dc9b8e879419a01064
Cameron Hunter <hello@cameronhunter.co.uk>
2012-09-22 22:27:19 +0100

    Removing dependency on AndroidCommon project

--- src/uk/co/cameronhunter/escalate/MainActivity.java
+++ src/uk/co/cameronhunter/escalate/MainActivity.java
@@ -1,13 +1,15 @@
 package uk.co.cameronhunter.escalate;
 
-import static uk.co.cameronhunter.adapters.booleanPreference.BooleanPreference.isChecked;
-import static uk.co.cameronhunter.utils.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
@@ -15,89 +17,105 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.RingtonePreference;
+import android.preference.TwoStatePreference;
 
 public class MainActivity extends PreferenceActivity {
 
-	@Override
-	public void onCreate( Bundle savedInstanceState ) {
-		super.onCreate( savedInstanceState );
+    @Override
+    public void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
 
-		if ( android.os.Build.VERSION.SDK_INT < 14 || getApplicationInfo().targetSdkVersion < 14 ) {
-			addPreferencesFromResource( R.xml.preferences );
-		} else {
-			addPreferencesFromResource( R.xml.preferences14 );
-		}
+        if ( android.os.Build.VERSION.SDK_INT < 14 ) {
+            addPreferencesFromResource( R.xml.preferences );
+        } else {
+            addPreferencesFromResource( R.xml.preferences14 );
+        }
 
-		RingtonePreference ringtone = (RingtonePreference) findPreference( getString( R.string.ringtone_key ) );
-		ringtone.setDefaultValue( RingtoneManager.getDefaultUri( RingtoneManager.TYPE_ALARM ).toString() );
+        RingtonePreference ringtone = (RingtonePreference) findPreference( getString( R.string.ringtone_key ) );
+        ringtone.setDefaultValue( RingtoneManager.getDefaultUri( RingtoneManager.TYPE_ALARM ).toString() );
 
-		EditTextPreference regex = (EditTextPreference) findPreference( getString( R.string.regex_key ) );
-		regex.setOnPreferenceChangeListener( new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange( Preference preference, Object newValue ) {
-				String pattern = (String) newValue;
-				if ( !isBlank( pattern ) && isValidPattern( pattern ) ) {
-					preference.setSummary( pattern );
-					return true;
-				}
-				return false;
-			}
-		} );
+        EditTextPreference regex = (EditTextPreference) findPreference( getString( R.string.regex_key ) );
+        regex.setOnPreferenceChangeListener( new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange( Preference preference, Object newValue ) {
+                String pattern = (String) newValue;
+                if ( isNotBlank( pattern ) && isValidPattern( pattern ) ) {
+                    preference.setSummary( pattern );
+                    return true;
+                }
+                return false;
+            }
+        } );
 
-		if ( !isBlank( regex.getText() ) ) {
-			regex.setSummary( regex.getText() );
-		}
+        if ( isNotBlank( regex.getText() ) ) {
+            regex.setSummary( regex.getText() );
+        }
 
-		final Preference onCallEnabled = findPreference( getString( R.string.on_call_key ) );
-		final CheckBoxPreference showReminder = (CheckBoxPreference) findPreference( getString( R.string.show_notification_key ) );
-		final EditTextPreference reminderMessage = (EditTextPreference) findPreference( getString( R.string.notification_message_key ) );
+        final Preference onCallEnabled = findPreference( getString( R.string.on_call_key ) );
+        final CheckBoxPreference showReminder = (CheckBoxPreference) findPreference( getString( R.string.show_notification_key ) );
+        final EditTextPreference reminderMessage = (EditTextPreference) findPreference( getString( R.string.notification_message_key ) );
 
-		OnPreferenceChangeListener onPreferenceChangeListener = new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange( Preference preference, Object newValue ) {
-				if ( Boolean.TRUE.equals( (Boolean) newValue && (isChecked( onCallEnabled ) || isChecked( showReminder )) ) ) {
-					Intent updateReminder = new Intent( getString( R.string.update_reminder_intent ) );
-					updateReminder.putExtra( getString( R.string.notification_message_key ), reminderMessage.getText() );
-					sendBroadcast( updateReminder );
-				} else {
-					sendBroadcast( new Intent( getString( R.string.remove_reminder_intent ) ) );
-				}
-				return true;
-			}
-		};
+        OnPreferenceChangeListener onPreferenceChangeListener = new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange( Preference preference, Object newValue ) {
+                if ( Boolean.TRUE.equals( (Boolean) newValue && (isChecked( onCallEnabled ) || isChecked( showReminder )) ) ) {
+                    Intent updateReminder = new Intent( getString( R.string.update_reminder_intent ) );
+                    updateReminder.putExtra( getString( R.string.notification_message_key ), reminderMessage.getText() );
+                    sendBroadcast( updateReminder );
+                } else {
+                    sendBroadcast( new Intent( getString( R.string.remove_reminder_intent ) ) );
+                }
+                return true;
+            }
+        };
 
-		onCallEnabled.setOnPreferenceChangeListener( onPreferenceChangeListener );
-		showReminder.setOnPreferenceChangeListener( onPreferenceChangeListener );
+        onCallEnabled.setOnPreferenceChangeListener( onPreferenceChangeListener );
+        showReminder.setOnPreferenceChangeListener( onPreferenceChangeListener );
 
-		reminderMessage.setOnPreferenceChangeListener( new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange( Preference preference, Object newValue ) {
-				String value = (String) newValue;
-				String message = isBlank( value ) ? getString( R.string.notification_message_default ) : value;
+        reminderMessage.setOnPreferenceChangeListener( new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange( Preference preference, Object newValue ) {
+                String value = (String) newValue;
+                String message = isBlank( value ) ? getString( R.string.notification_message_default ) : value;
 
-				preference.setSummary( message );
+                preference.setSummary( message );
 
-				if ( isChecked( onCallEnabled ) && isChecked( showReminder ) ) {
-					Intent updateReminder = new Intent( getString( R.string.update_reminder_intent ) );
-					updateReminder.putExtra( getString( R.string.notification_message_key ), message );
-					sendBroadcast( updateReminder );
-				}
+                if ( isChecked( onCallEnabled ) && isChecked( showReminder ) ) {
+                    Intent updateReminder = new Intent( getString( R.string.update_reminder_intent ) );
+                    updateReminder.putExtra( getString( R.string.notification_message_key ), message );
+                    sendBroadcast( updateReminder );
+                }
 
-				return true;
-			}
-		} );
+                return true;
+            }
+        } );
 
-		reminderMessage.setSummary( isBlank( reminderMessage.getText() ) ? getString( R.string.notification_message_default ) : reminderMessage.getText() );
+        reminderMessage.setSummary( isBlank( reminderMessage.getText() ) ? getString( R.string.notification_message_default ) : reminderMessage.getText() );
 
-		if ( isChecked( onCallEnabled ) && isChecked( showReminder ) ) {
-			sendBroadcast( new Intent( getString( R.string.update_reminder_intent ) ) );
-		}
-	}
+        if ( isChecked( onCallEnabled ) && isChecked( showReminder ) ) {
+            sendBroadcast( new Intent( getString( R.string.update_reminder_intent ) ) );
+        }
+    }
 
-	private static boolean isValidPattern( String pattern ) {
-		try {
-			Pattern.compile( pattern );
-			return true;
-		} catch ( PatternSyntaxException ex ) {
-			return false;
-		}
-	}
+    private static boolean isValidPattern( String pattern ) {
+        try {
+            Pattern.compile( pattern );
+            return true;
+        } catch ( PatternSyntaxException ex ) {
+            return false;
+        }
+    }
+
+    @SuppressLint( "NewApi" )
+    private static boolean isChecked( Preference preference ) {
+        if ( Build.VERSION.SDK_INT >= 14 ) {
+            if ( preference instanceof TwoStatePreference ) {
+                return ((TwoStatePreference) preference).isChecked();
+            }
+        }
+
+        if ( preference instanceof CheckBoxPreference ) {
+            return ((CheckBoxPreference) preference).isChecked();
+        }
+
+        throw new RuntimeException( "Not supported preference" );
+    }
 
 }
--- src/uk/co/cameronhunter/escalate/ReminderReceiver.java
+++ src/uk/co/cameronhunter/escalate/ReminderReceiver.java
@@ -2,8 +2,7 @@ package uk.co.cameronhunter.escalate;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
 import static android.content.Intent.ACTION_BOOT_COMPLETED;
-import static uk.co.cameronhunter.utils.StringUtils.isBlank;
-import uk.co.cameronhunter.adapters.notificationBuilder.NotificationBuilder;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -11,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 
 public class ReminderReceiver extends BroadcastReceiver {
 
@@ -58,14 +58,14 @@ public class ReminderReceiver extends BroadcastReceiver {
 	}
 
 	private void updateReminderNotification( NotificationManager notificationManger, String message ) {
-		NotificationBuilder builder = NotificationBuilder.create( context );
+	    NotificationCompat.Builder builder = new NotificationCompat.Builder( context );
 
 		builder.setContentTitle( isBlank( message ) ? context.getString( R.string.notification_message_default ) : message );
 		builder.setSmallIcon( android.R.drawable.ic_dialog_info );
 		builder.setContentIntent( PendingIntent.getActivity( context, 0, new Intent( context, MainActivity.class ), 0 ) );
 		builder.setOngoing( true );
 
-		notificationManger.notify( "reminder", 1, builder.getNotification() );
+		notificationManger.notify( "reminder", 1, builder.build() );
 	}
 
 	private void removeReminderNotification(NotificationManager notificationManger) {
